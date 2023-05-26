dba8df882f0b3ceb2303c2e99051b8d0b23180a3
Manu Puyuelo <puyuelo.m@gmail.com>
2023-05-27 01:28:56 +0200

    first page done for desktop

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+
+var app = express();
+
 const cors = require("cors");
 app.use(cors());
-var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
